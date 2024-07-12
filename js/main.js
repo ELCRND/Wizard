69a139fb5e37a3ff808a53fe92526d384afbe17d
ELCRND <jwhh2a@gmail.com>
2024-07-12 19:13:27 +0300

    new file:   css/styles.css 	new file:   css/styles.css.map 	new file:   index.html 	new file:   js/main.js 	new file:   package.json 	new file:   public/fonts/ink-free.ttf 	new file:   public/fonts/tolkiencyr.ttf 	new file:   public/img/about/about_bg.png 	new file:   public/img/about/about_img.png 	new file:   public/img/about/about_img_mobile.png 	new file:   public/img/buy/buy_bg.png 	new file:   public/img/buy/buy_img.png 	new file:   public/img/buy/buy_img_mobile.png 	new file:   public/img/footer/footer_img.png 	new file:   public/img/home/home_bg.png 	new file:   public/img/home/home_butterfly_1.png 	new file:   public/img/home/home_butterfly_2.png 	new file:   public/img/home/home_butterfly_3.png 	new file:   public/img/home/home_content_blur.png 	new file:   public/img/home/home_content_sepia.png 	new file:   public/img/home/home_decoration.png 	new file:   public/img/home/home_decoration_mobile.png 	new file:   public/img/home/home_dwarf.png 	new file:   public/img/home/home_dwarf_mobile.png 	new file:   public/img/home/home_logo.png 	new file:   public/img/home/home_logo_mobile.png 	new file:   public/img/tokenomics/statistics_bg.png 	new file:   public/img/tokenomics/tokenomics_batterfly.png 	new file:   public/img/tokenomics/tokenomics_img.png 	new file:   public/img/tokenomics/tokenomics_img_mobile.png 	new file:   scss/base/_base.scss 	new file:   scss/base/_reset.scss 	new file:   scss/blocks/about.scss 	new file:   scss/blocks/buy.scss 	new file:   scss/blocks/footer.scss 	new file:   scss/blocks/header.scss 	new file:   scss/blocks/home.scss 	new file:   scss/blocks/tokenomics.scss 	new file:   scss/main.scss

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const menuBtn = document.querySelector(".header__menu-btn");
+const menu = document.querySelector(".header__menu");
+// const aboutKey = document.querySelector("#about-key");
+// const tooltip = document.querySelector(".tooltip");
+
+// открыть/закрыть меню
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("visible");
+  menuBtn.classList.toggle("close");
+  document.body.classList.toggle("scroll-off");
+});
+
+// закрытие меню при нажатии на ссылку
+// document.querySelectorAll(".menu__link").forEach((link) =>
+//   link.addEventListener("click", () => {
+//     menu.classList.remove("visible");
+//     menuBtn.classList.remove("close");
+//     document.body.classList.remove("scroll-off");
+//   })
+// );
+
+// копирование кода в буфер обмена
+// document.querySelector("#about-key-copy").addEventListener("click", () => {
+//   navigator.clipboard.writeText(aboutKey.innerText);
+//   tooltip.classList.add("visible");
+//   setTimeout(() => tooltip.classList.remove("visible"), 400);
+// });
+
+// document.querySelector("#footer-key-copy").addEventListener("click", () => {
+//   navigator.clipboard.writeText(footerKey.innerText);
+//   tooltip.classList.add("visible");
+//   setTimeout(() => tooltip.classList.remove("visible"), 400);
+// });
